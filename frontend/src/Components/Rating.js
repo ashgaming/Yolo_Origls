81f4f76b1dab4d7f8c8b3bcc4e4af97e75ff9092
Ashish Alhat <68996995+ashgaming@users.noreply.github.com>
2024-02-23 12:06:47 +0530

    editing

--- frontend/src/Components/Rating.js
+++ frontend/src/Components/Rating.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import 'react-bootstrap'
+<<<<<<< Updated upstream
 import 'bootstrap'
+=======
+import 'ref'
+>>>>>>> Stashed changes
 
-export default function Rating({value,text,clr}) {
+export default function Rating({value,text,color}) {
   return (
     <div className='rating'>
-
         <span>
+<<<<<<< Updated upstream
             <i style={{Color:clr}} className={
                 value >= 1 ? 'fas fa-star':
                  value >= 0.5 ?'fas fa-star-half-alt' :
@@ -16,6 +20,20 @@ export default function Rating({value,text,clr}) {
         </span>
         <span>
             <i style={{Color:clr}}  className={
+=======
+            <i  style={color} className={
+                value >=1 ?
+                'fas fa-star'
+                : value >= 0.5
+                ?'fas fa-star-half-alt'
+                :'far fa-star'
+            }>
+            </i>
+            <span>{value}</span>
+        </span>
+        <span>
+            <i Style={color} className={
+>>>>>>> Stashed changes
                 value>=2 ?
                 'fas fa-star'
                 : value >= 1.5
@@ -25,7 +43,7 @@ export default function Rating({value,text,clr}) {
             </i>
         </span>
         <span>
-            <i style={{color:clr}}  className={
+            <i Style={color} className={
                 value>=3 ?
                 'fas fa-star'
                 : value >= 2.5
@@ -35,7 +53,7 @@ export default function Rating({value,text,clr}) {
             </i>
         </span>
         <span>
-            <i style={{color:clr}}  className={
+            <i Style={color} className={
                 value>=4 ?
                 'fas fa-star'
                 : value >= 3.5
@@ -45,7 +63,7 @@ export default function Rating({value,text,clr}) {
             </i>
         </span>
         <span>
-            <i style={{color:clr}}  className={
+            <i Style={color} className={
                 value>=5 ?
                 'fas fa-star'
                 : value >= 4.5
@@ -53,10 +71,14 @@ export default function Rating({value,text,clr}) {
                 :'far fa-star'
             }>
             </i>
+<<<<<<< Updated upstream
             
          </span>
         
         <span>{value} Rating from {text} Reviews</span>
+=======
+        </span>
+>>>>>>> Stashed changes
     </div>
   )
 }
