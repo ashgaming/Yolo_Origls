c99b2aeda6a518288c28fbb41e3bb61b25c23cc9
ashgaming <68996995+ashgaming@users.noreply.github.com>
2024-02-29 20:34:40 +0530

    restored files

--- backends/backends/settings.py
+++ backends/backends/settings.py
@@ -171,14 +171,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-MEDIA_ROOT = 'static/images/'
+MEDIA_ROOT = 'backends/static/images'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
