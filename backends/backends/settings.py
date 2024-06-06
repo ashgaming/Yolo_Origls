042b7eacfd92151302aa2bfad7721bb9838d6969
ashgaming <68996995+ashgaming@users.noreply.github.com>
2024-06-06 21:56:21 +0530

    deplay 11

--- backends/backends/settings.py
+++ backends/backends/settings.py
@@ -21,9 +21,6 @@ env = environ.Env()
 environ.Env.read_env()
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
